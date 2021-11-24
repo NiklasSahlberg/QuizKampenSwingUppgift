4fc8e0321d7c84ffa64fba2d1401623e58eb7153
NiklasSahlberg <Niklassahlberg@hotmail.com>
2021-11-24 22:17:17 +0100

    Initial commit

---
+++ src/Default/Main.java
@@ -0,0 +1,14 @@
+package Default;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new QuestionsAndRounds();
+        //new LoadQuestionsAndRounds();
+        new GameSettings();
+	//new Quiz();
+    }
+}
